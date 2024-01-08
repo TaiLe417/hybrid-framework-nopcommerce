8c4e773059e504aa109bb9aa84a68506c8d641ab
taile47711 <taile47711@gmail.com>
2024-01-08 22:01:44 +0700

    Update LoginPageObject

---
+++ hybrid-framework-nopcommerce/actions/pageObjects/LoginPageObject.java
@@ -0,0 +1,38 @@
+package pageObjects;
+
+import commons.BasePage;
+import org.openqa.selenium.WebDriver;
+import pageUIs.LoginPageUI;
+
+public class LoginPageObject extends BasePage {
+    WebDriver driver;
+
+    public LoginPageObject(WebDriver drive) {
+        this.driver = drive;
+    }
+
+    public void clickLogInButton() {
+        waitForElementClickable(driver, LoginPageUI.LOGIN_BUTTON);
+        clickToElement(driver, LoginPageUI.LOGIN_BUTTON);
+    }
+
+    public void inputToEmailTextBox(String emailAddress) {
+        waitForElementVisible(driver, LoginPageUI.EMAIL_TEXTBOX);
+        sendKeyToElement(driver, LoginPageUI.EMAIL_TEXTBOX, emailAddress);
+    }
+
+    public void inputToPasswordTextBox(String password) {
+        waitForElementVisible(driver, LoginPageUI.PASSWORD_TEXTBOX);
+        sendKeyToElement(driver, LoginPageUI.PASSWORD_TEXTBOX, password);
+    }
+
+    public String getEmailErrorMessage() {
+        waitForElementVisible(driver, LoginPageUI.EMAIL_ERROR_MESSAGE);
+        return getTextElement(driver, LoginPageUI.EMAIL_ERROR_MESSAGE);
+    }
+
+    public String getUnsuccessfulErrorMessage() {
+        waitForElementVisible(driver, LoginPageUI.UNSUCCESSFUL_ERROR_MESSAGE);
+        return getTextElement(driver, LoginPageUI.UNSUCCESSFUL_ERROR_MESSAGE);
+    }
+}
