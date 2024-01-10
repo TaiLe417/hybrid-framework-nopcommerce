d51f06968018d7022360c9171318b3c40bc12b82
taile47711 <taile47711@gmail.com>
2024-01-10 22:37:02 +0700

    Refactor code

--- hybrid-framework-nopcommerce/actions/commons/BaseTest.java
+++ hybrid-framework-nopcommerce/actions/commons/BaseTest.java
@@ -2,14 +2,9 @@ package commons;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
 import java.time.Duration;
 
 public class BaseTest {
-    private final String projectPath = System.getProperty("user.dir");
     private WebDriver driver;
 
     protected WebDriver getBrowserName(String browserName) {
