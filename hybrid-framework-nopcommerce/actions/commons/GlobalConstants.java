5fe2fc2607b66c3800455e4ce6edd88f37a0f343
taile47711 <taile47711@gmail.com>
2024-02-06 22:29:50 +0700

    refactor code

--- hybrid-framework-nopcommerce/actions/commons/GlobalConstants.java
+++ hybrid-framework-nopcommerce/actions/commons/GlobalConstants.java
@@ -8,7 +8,8 @@ public class GlobalConstants {
     public static final String PROJECT_PATH = System.getProperty("user.dir");
     public static final String UPLOAD_FILE = PROJECT_PATH + File.separator + "hybrid-framework-nopcommerce" + File.separator + "uploadFiles" + File.separator;
     public static final String REPORTNG_SCREENSHOT = PROJECT_PATH + File.separator + "ReportNGScreenShots" + File.separator;
-    public static final String EXTENT_PATH = PROJECT_PATH + File.separator + "hybrid-framework-nopcommerce" + File.separator + "extendReportV2" + File.separator;
+    public static final String EXTENT_PATH = PROJECT_PATH + File.separator + "hybrid-framework-nopcommerce" + File.separator + "extentReportV2" + File.separator;
     public static final int LONG_TIMEOUT = 30;
     public static final int SHORT_TIMEOUT = 3;
+    public static final String JAVA_VERSION = System.getProperty("java.version");
 }
